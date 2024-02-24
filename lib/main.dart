3f646a1afaee386846db4d0658b87cd661ff6c34
ivan-horchakov <ivan.horchakov@gmail.com>
2024-02-24 14:19:07 +0100

    FirebaseCLI & SDK (273)

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,13 @@
+import 'package:chat_app/screens/auth.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const App());
 }
 
@@ -12,9 +19,11 @@ class App extends StatelessWidget {
     return MaterialApp(
       title: 'FlutterChat',
       theme: ThemeData().copyWith(
-        colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 63, 17, 177)),
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: const Color.fromRGBO(0, 188, 212, 1),
+        ),
       ),
-      home: Scaffold(),
+      home: const AuthScreen(),
     );
   }
 }
